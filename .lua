c04e958a14ec28fb2743a54cf9b89c14961cf177
KunOkazure <duckduck090512@gmail.com>
2024-12-01 12:19:15 +0700

    Oka.lua

--- .lua
+++ .lua
@@ -3,7 +3,7 @@ local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/d
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
 ----------------------------------------------------------------------------------------------------------------------------------------------
 local Window = Fluent:CreateWindow({
-    Title = "Banana Hub",
+    Title = "OkaChp Hub",
     SubTitle = "True V2 | By Oka",
     TabWidth = 160,
     Size = UDim2.fromOffset(500, 320),
